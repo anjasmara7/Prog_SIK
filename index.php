02cba30ac97ddf2e41e03b3c093a9e751a0530bc
anjasmara7 <paradaanjas@gmail.com>
2022-03-03 16:44:56 +0700

    ok

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             $halaman=$opsi;
             if($halaman==''){
                 $halaman=='index';
-                require_once (PUB_DIR . 'sign-in.php');
+                require_once (PUB_DIR . 'signin.php');
             }else{
                 $namafile = $halaman.'.php';
                 if(file_exists($namafile)){
